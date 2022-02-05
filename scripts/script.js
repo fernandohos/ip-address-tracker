af0eb7bcf509b8ae4896fd249c442079401383aa
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-02-05 19:59:48 -0300

    :sparkles: fetch and display data

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+const form = qs("#form");
+const input = qs("#input");
+
+function qs(queryString) {
+    return document.querySelector(queryString);
+}
+
+async function handleSubmit(e) {
+    e.preventDefault();
+    isNaN(input.value);
+    const res = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${config.apiKey}&${isNaN(input.value) ? "domain" : "ipAddress"}=${input.value}`).then(r => r.json());
+    const {ip, location, isp} = res;
+    insertDataOnPage(ip, location, isp);
+}
+
+function insertDataOnPage(ip, location, isp) {
+    qs("#ip").innerText = ip;
+    qs("#isp").innerText = isp;
+    qs("#timezone").innerText = `UTC ${location.timezone}`;
+    qs("#location").innerText = `${location.region}, ${location.country}`;
+
+}
+
+form.addEventListener("submit", handleSubmit);
