8457c3113fe47e37cf67bb5e6e495b723d8fcbe9
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-02-07 16:38:01 -0300

    :sparkles: build map on submit form

--- scripts/script.js
+++ scripts/script.js
@@ -8,16 +8,17 @@ function qs(queryString) {
 async function handleSubmit(e) {
     e.preventDefault();
     isNaN(input.value);
-    const res = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${config.apiKey}&${isNaN(input.value) ? "domain" : "ipAddress"}=${input.value}`).then(r => r.json());
+    const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${config.apiKey}&${isNaN(input.value) ? "domain" : "ipAddress"}=${input.value}`).then(r => r.json());
     const {ip, location, isp} = res;
     insertDataOnPage(ip, location, isp);
+    getLatLngFromIp(ip).then(latlng => buildMap(...latlng));
 }
 
 function insertDataOnPage(ip, location, isp) {
     qs("#ip").innerText = ip;
     qs("#isp").innerText = isp;
     qs("#timezone").innerText = `UTC ${location.timezone}`;
-    qs("#location").innerText = `${location.region}, ${location.country}`;
+    qs("#location").innerText = `${location.city}, ${location.region} - ${location.country}`;
 
 }
 
